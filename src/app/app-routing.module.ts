32581503103fb36d372525cc485f4aa60b4c899d
mukesh <mkvaidya2000@gmail.com>
2019-04-30 23:23:47 +0530

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,6 +22,7 @@ const routes: Routes = [
 
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  
   { path: '**',   component: PagenotfoundComponent }
 ];
 
